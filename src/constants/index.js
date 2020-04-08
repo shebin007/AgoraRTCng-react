e4e7ee57fe2d9bf2eaefa1e3237b3c3740e22978
shebin007 <shebinkp7@gmail.com>
2020-04-08 23:28:05 +0530

    intial commit

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "22e48a2d0a9e429c99e3d262720e036e",
+  // Pass a token if your project enables the App Certificate
+  token: null,
+};
